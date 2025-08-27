8c0f3b6cda035136a66e0fd43001037a1e004fc7
M. Nura Alam Naim <nuraalomnaim@gmail.com>
2025-08-28 01:03:47 +0600

    Empty Constructor in T_users

--- app/src/main/java/edu/ewubd/doctorrank223410/T_Users.java
+++ app/src/main/java/edu/ewubd/doctorrank223410/T_Users.java
@@ -1,90 +1,15 @@
 package edu.ewubd.doctorrank223410;
 
 public class T_Users {
-    public String name, email,phone,password, gender, dob, height, weight,  image, UID;
+    public String name, email, phone, password, gender, dob, height, weight, image, uid;
 
-    public String getName() {
-        return name;
+    public T_Users() {
+        // Default constructor required for calls to DataSnapshot.getValue(T_Users.class)
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getHeight() {
-        return height;
-    }
-
-    public void setHeight(String height) {
-        this.height = height;
-    }
-
-    public String getWeight() {
-        return weight;
-    }
-
-    public void setWeight(String weight) {
-        this.weight = weight;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getUID() {
-        return UID;
-    }
-
-    public void setUID(String UID) {
-        this.UID = UID;
-    }
-    public T_Users() {}
-    public T_Users(String name, String email, String phone, String password, String gender, String dob, String height, String weight, String  image, String UID)
-    {
+    public T_Users(String name, String email, String phone, String password,
+                   String gender, String dob, String height, String weight,
+                   String image, String uid) {
         this.name = name;
         this.email = email;
         this.phone = phone;
@@ -94,7 +19,37 @@ public class T_Users {
         this.height = height;
         this.weight = weight;
         this.image = image;
-        this.UID = UID;
+        this.uid = uid;
     }
 
+    // --- Getters and Setters ---
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getDob() { return dob; }
+    public void setDob(String dob) { this.dob = dob; }
+
+    public String getHeight() { return height; }
+    public void setHeight(String height) { this.height = height; }
+
+    public String getWeight() { return weight; }
+    public void setWeight(String weight) { this.weight = weight; }
+
+    public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
+
+    public String getUid() { return uid; }
+    public void setUid(String uid) { this.uid = uid; }
 }
