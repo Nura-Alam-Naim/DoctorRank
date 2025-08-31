b45f685b404f347c2da1cdd0b7473202b68b1cd2
M. Nura Alam Naim <nuraalomnaim@gmail.com>
2025-08-31 15:07:26 +0600

    Final Changes Done

--- app/src/main/java/edu/ewubd/doctorrank223410/MainActivity.java
+++ app/src/main/java/edu/ewubd/doctorrank223410/MainActivity.java
@@ -4,18 +4,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.icu.text.SimpleDateFormat;
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.graphics.Color;
+import android.widget.AutoCompleteTextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,113 +25,129 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;   // use java.text
 import java.util.ArrayList;
+import java.util.Calendar;           // use java.util
 import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView ivProfilePicture ;
-    private TextView tvDate, tvDay,  tvNoAppointments, tvLog;
-    private ListView lvDoctorList ;
-    private DoctorListAdapter DocList;
-    private LinearLayout SetVisible;
-    private Button btProfile, btUpcomingAppointments, btBookPreferedDoc;
+    private ImageView ivProfilePicture;
+    private TextView tvDate, tvDay, tvNoAppointments, tvLog;
+    private ListView lvDoctorList;
+    private Button btProfile, btUpcomingAppointments, btTodayAppointment, btLogout;
     private SharedPreferences sp;
-    private AppointmentListAdapter todayAdapter;
-    private ArrayList<UserBooking> todayAppointments = new ArrayList<>();
+
+    private SearchView searchView;
+    DoctorListAdapter adapter;
+    private ArrayList<T_DoctorInfo> doctorInfoList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ivProfilePicture = findViewById(R.id.ivProfilePicture) ;
-        tvDate = findViewById(R.id.tvDate) ;
-        tvDay = findViewById(R.id.tvDay) ;
-        lvDoctorList = findViewById(R.id.lvDoctorList) ;
-        btProfile = findViewById(R.id.btProfile) ;
-        btUpcomingAppointments = findViewById(R.id.btUpcomingAppointments) ;
-        btBookPreferedDoc = findViewById(R.id.btBookPreferedDoc) ;
-        tvNoAppointments = findViewById(R.id.tvNoAppointments);
-        tvLog=findViewById(R.id.tvLog);
-        SetVisible = findViewById(R.id.SetVisible);
+
+        ivProfilePicture = findViewById(R.id.ivProfilePicture);
+        tvDate = findViewById(R.id.tvDate);
+        tvDay = findViewById(R.id.tvDay);
+        lvDoctorList = findViewById(R.id.lvDoctorList);
+        btProfile = findViewById(R.id.btProfile);
+        btUpcomingAppointments = findViewById(R.id.btUpcomingAppointments);
+        btTodayAppointment = findViewById(R.id.btTodayAppointment);
+        btLogout =findViewById(R.id.btLogout);
+        searchView = findViewById(R.id.doctorSearchView);
+
         setDateAndDay();
+
+        DoctorsDB db = DoctorsDB.get(this);
+        doctorInfoList = db.GetAll();
+        adapter = new DoctorListAdapter(this, doctorInfoList);
+        lvDoctorList.setAdapter(adapter);
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.getFilter().filter(query);
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                finishAffinity();
+            }
+        });
+
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if(currentUser != null)
-        {
-            tvLog.setVisibility(View.GONE);
-            SetVisible.setVisibility(View.VISIBLE);
-            todayAdapter = new AppointmentListAdapter(this, todayAppointments);
-            lvDoctorList.setAdapter(todayAdapter);
-            loadTodaysAppointments(currentUser.getUid());
-        }
-        else
-        {
-            tvLog.setVisibility(View.VISIBLE);
-            SetVisible.setVisibility(View.GONE);
-        }
+
         if (currentUser != null) {
             btProfile.setText("Profile");
-            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child(currentUser.getUid());
+            DatabaseReference ref = FirebaseDatabase.getInstance()
+                    .getReference("users")
+                    .child(currentUser.getUid());
             ref.get().addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
                     T_Users user = task.getResult().getValue(T_Users.class);
-                    if (user != null) {
-                        if (user.image != null && !user.image.isEmpty()) {
-                            try {
-                                byte[] bytes = Base64.decode(user.image, Base64.DEFAULT);
-                                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                                ivProfilePicture.setImageBitmap(bmp);
-                            } catch (Exception e) {
-                                ivProfilePicture.setImageResource(R.drawable.dummy);
-                            }
+                    if (user != null && user.image != null && !user.image.isEmpty()) {
+                        try {
+                            byte[] bytes = Base64.decode(user.image, Base64.DEFAULT);
+                            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                            ivProfilePicture.setImageBitmap(bmp);
+                        } catch (Exception e) {
+                            ivProfilePicture.setImageResource(R.drawable.dummy);
                         }
-                    }
-                    else
-                    {
+                    } else {
                         ivProfilePicture.setImageResource(R.drawable.dummy);
                     }
                 }
             });
+            btLogout.setVisibility(View.VISIBLE);
         } else {
             btProfile.setText("Login");
+            btLogout.setVisibility(View.INVISIBLE);
         }
-        btProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (currentUser != null) {
-                    startActivity(new Intent(MainActivity.this, UserProfilePage.class));
-                } else {
-                    startActivity(new Intent(MainActivity.this, LoginPage.class));
-                }
+
+        btProfile.setOnClickListener(v -> {
+            if (currentUser != null) {
+                startActivity(new Intent(MainActivity.this, UserProfilePage.class));
+            } else {
+                startActivity(new Intent(MainActivity.this, LoginPage.class));
             }
         });
-        btUpcomingAppointments.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(currentUser != null)
-                {
-                    Intent i=new Intent(MainActivity.this, UpcomingAppointment.class);
-                    startActivity(i);
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "Please Login First", Toast.LENGTH_SHORT).show();
-                    Intent i=new Intent(MainActivity.this, LoginPage.class);
-                    startActivity(i);
-                }
+
+        btUpcomingAppointments.setOnClickListener(v -> {
+            if (currentUser != null) {
+                Intent i = new Intent(MainActivity.this, UpcomingAppointment.class);
+                startActivity(i);
+            } else {
+                Toast.makeText(getApplicationContext(), "Please Login First", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, LoginPage.class));
             }
         });
-        btBookPreferedDoc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this, DoctorSelectionPage.class);
+
+        btTodayAppointment.setOnClickListener(v -> {
+            if (currentUser != null) {
+                Intent i = new Intent(MainActivity.this, DoctorSelectionPage.class);
                 startActivity(i);
             }
+            else {
+                Toast.makeText(getApplicationContext(), "Please Login First", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, LoginPage.class));
+            }
+
         });
     }
-    void setDateAndDay()
-    {
+
+    void setDateAndDay() {
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy", Locale.getDefault());
         String currentDate = dateFormat.format(calendar.getTime());
@@ -139,51 +156,4 @@ public class MainActivity extends AppCompatActivity {
         tvDate.setText(currentDate);
         tvDay.setText(currentDay);
     }
-    private void loadTodaysAppointments(String userId) {
-        DatabaseReference ref = FirebaseDatabase.getInstance()
-                .getReference("user_bookings")
-                .child(userId);
-
-        ref.get().addOnSuccessListener(snapshot -> {
-            ArrayList<UserBooking> localList = new ArrayList<>();
-
-            java.util.Calendar cal = java.util.Calendar.getInstance();
-            cal.set(java.util.Calendar.HOUR_OF_DAY, 0);
-            cal.set(java.util.Calendar.MINUTE, 0);
-            cal.set(java.util.Calendar.SECOND, 0);
-            cal.set(java.util.Calendar.MILLISECOND, 0);
-            Date today = cal.getTime();
-
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-
-            for (DataSnapshot child : snapshot.getChildren()) {
-                UserBooking booking = child.getValue(UserBooking.class);
-                if (booking != null) {
-                    try {
-                        Date bookingDate = sdf.parse(booking.date);
-                        if (bookingDate != null && bookingDate.equals(today)) {
-                            localList.add(booking);
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            todayAppointments.clear();
-            todayAppointments.addAll(localList);
-            todayAdapter.notifyDataSetChanged();
-
-            if (todayAppointments.isEmpty()) {
-                tvNoAppointments.setVisibility(View.VISIBLE);
-                lvDoctorList.setVisibility(View.GONE);
-            } else {
-                tvNoAppointments.setVisibility(View.GONE);
-                lvDoctorList.setVisibility(View.VISIBLE);
-            }
-
-        }).addOnFailureListener(e ->
-                Toast.makeText(this, "Failed to load today's appointments: " + e.getMessage(), Toast.LENGTH_SHORT).show()
-        );
-    }
-
 }
